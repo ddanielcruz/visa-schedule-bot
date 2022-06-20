9f069b443e7852b47ac1b888d1089c0a01e2bba1
Daniel Cruz <danielcunha54@gmail.com>
2022-06-20 20:58:14 -0300

    Reuse browser between extractions

--- src/program.js
+++ src/program.js
@@ -16,6 +16,8 @@ const scheduleUrl = `${baseUrl}/schedule/${SCHEDULE_ID}/appointment/days/${CONSU
 const maximumDate = new Date(2022, 7, 31)
 puppeteer.use(StealthPlugin())
 
+let browser, page, launchedAt
+
 async function retrieveConsulateDates(page, retry = true) {
   const response = await page.goto(scheduleUrl)
   const availableDates = await response.json()
@@ -45,45 +47,58 @@ async function notifyClosestDates(dates) {
   }
 }
 
-async function extractDates() {
-  // Launch browser and navigate to VISA page
-  logger.debug('Navigating to schedule application')
-  const browser = await puppeteer.launch({ headless: HEADLESS === 'true' })
+async function launchBrowserAndSign() {
+  // If browser was already launched, check if it's older than 15 minutes to launch again
+  if (browser) {
+    const diffMins = Math.round((Math.abs(Date.now() - launchedAt) / (1000 * 60)) % 60)
+    if (diffMins >= 15) {
+      logger.debug('Closing browser after 15 minutes')
+      await browser.close()
+      page = undefined
+      browser = undefined
+    }
+  }
 
-  try {
-    const [page] = await browser.pages()
+  // Launch a new browser if it's closed
+  if (!browser) {
+    logger.debug('Navigating to schedule application')
+    launchedAt = Date.now()
+    browser = await puppeteer.launch({ headless: HEADLESS === 'true' })
+    page = (await browser.pages())[0]
     await page.goto(signInUrl)
 
-    // Populate user credentials and sign in
     logger.debug('Signing in to user profile')
     await page.waitForSelector('#user_email')
     await page.type('#user_email', EMAIL)
     await page.type('#user_password', PASSWORD)
     await page.click('#policy_confirmed')
     await page.click('.new_user input.button')
-
-    // Request schedule available dates and log the five closest ones
-    logger.debug(`Requesting available dates from consulate ${chalk.green(CONSULATE)}`)
     await page.waitForSelector('.attend_appointment')
+  }
+}
+
+async function extractDates() {
+  try {
+    await launchBrowserAndSign()
+
+    logger.debug(`Requesting available dates from consulate ${chalk.green(CONSULATE)}`)
     let availableDates = await retrieveConsulateDates(page)
     availableDates = lodash.sortBy(availableDates, 'date')
 
     if (availableDates.length) {
-      logger.info(`Closest dates in the selected consulate:`)
-      const closestDates = availableDates.slice(0, 5)
+      logger.info('Closest dates in the selected consulate:')
+      const closestDates = availableDates.slice(0, 3)
       closestDates.forEach(({ date }) => logger.info(date))
       notifyClosestDates(closestDates)
     } else {
       logger.error("The consulate doesn't have any available date!")
     }
   } catch (error) {
-    logger.error(error.message)
-  } finally {
-    await browser.close()
+    logger.error(error)
   }
 }
 
-logger.warn(`Scheduling bot to run every ${INTERVAL} seconds.`)
+logger.warn(`Scheduling bot to run every ${chalk.green(INTERVAL)} seconds.`)
 logger.warn(`Maximum date is set to ${maximumDate.toDateString()}`)
 
 const interval = parseInt(INTERVAL) * 1000
